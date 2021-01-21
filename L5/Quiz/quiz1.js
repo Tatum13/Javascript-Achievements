b915e98addfd10c95b84111d00ee0adeb11bdecd
tatum <tatumdv2001@gmail.com>
2021-01-22 00:01:22 +0100

    turtle

---
+++ L5/Quiz/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Achievement",
+        "imageURI": "naruto.jpg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Wat is mijn favoriete serie?",
+				"answers": [{
+						"answer": "Once upon a time",
+						"feedback": true
+					},
+					{
+						"answer": "Game of thrones",
+						"feedback": false
+					},
+					{
+						"answer": "How I met your mother",
+						"feedback": true
+					},
+					{
+						"answer": "La casa de papel",
+						"feedback": false
+					},
+					{
+						"answer": "Stranger things",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is mijn favoriete kleur?",
+				"answers": [{
+						"answer": "Groen",
+						"feedback": false
+					},
+					{
+						"answer": "Zwart",
+						"feedback": true
+					},
+					{
+						"answer": "Roze",
+						"feedback": false
+					},
+					{
+						"answer": "Geel",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Wat is mijn favoriete seizoen ?",
+				"answers": [{
+						"answer": "Lente",
+						"feedback": false
+					},
+					{
+						"answer": "Zomer",
+						"feedback": false
+					},
+					{
+						"answer": "Herfst",
+						"feedback": false
+					},
+					{
+						"answer": "Winter",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
---
+++ L5/Quiz/quiz2.js
@@ -0,0 +1,65 @@
+const quiz2 = 
+{
+    "quizID": "Math 1",
+    "quizMetaData":
+    {
+        "title": "Achievement-2",
+        "imageURI": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/x3y3z3k-1568041112.jpg?crop=0.987xw:0.790xh;0.00160xw,0.128xh&resize=980:*"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "Welke achievement is dit?  ",
+				"answers": [{
+						"answer": "text to speech",
+						"feedback": false
+					},
+					{
+						"answer": "turtle",
+						"feedback": false
+					},
+					{
+						"answer": "Quiz",
+						"feedback": true
+					},
+					{
+						"answer": "Flexbox",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Welke film vind ik vreselijk?",
+				"answers": [{
+						"answer": "Harry Potter",
+						"feedback": false
+					},
+					{
+						"answer": "THE HUNT",
+						"feedback": true
+					},
+					{
+						"answer": "The Hunger games",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "Waar ben ik toe aan?",
+				"answers": [{
+						"answer": "Javascript",
+						"feedback": false
+					},
+					{
+						"answer": "Voedertijd",
+						"feedback": false
+					},
+					{
+						"answer": "Vakantie",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
